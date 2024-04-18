949b5df5344a1c99712baa85f93341cb88f4a6b2
unknown <jefersondeoliveira062@gmail.com>
2024-04-18 12:52:30 -0300

    feat: Cria componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <>
+    
+    <body className=" bg-bg w-full h-full">
+      <Header/>
       <h1 className=' text-2xl'>Hello world</h1>
-    </>
+    </body>
+  
   )
 }
 
